455e9975f922336fb0e5a40fc9b829e888dfc9d1
IsaacMartos <77277945+IsaacMartos@users.noreply.github.com>
2023-11-02 00:12:42 +0100

    Update PlayerMovement.cs

--- Assets/Code/Player/PlayerMovement.cs
+++ Assets/Code/Player/PlayerMovement.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
     public float speed = 10;
-    void Start()
-    {
-        
-    }
-
+    
     // Update is called once per frame
     void Update()
     {
+        OnPlayerMovement();
+    }
+
+    void OnPlayerMovement()
+    {
+        if (GameManager.Instance.state == GameManager.GameState.Dialogue) return;
         if (Input.GetKey(KeyCode.A))
         {
             transform.position += new Vector3(-1, 0, 0) * (speed * Time.deltaTime);
