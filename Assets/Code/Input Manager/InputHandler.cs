72c5cc422a36f4b2c74b84a7a5c24c3dab6a29e6
Lucas Montero Pedrosa <57368590+Lucas-Montero-42@users.noreply.github.com>
2023-11-08 17:56:44 +0100

    Tarjeta para items + bloqueo de cursor al dialogar y al inspecionar objetos

--- Assets/Code/Input Manager/InputHandler.cs
+++ Assets/Code/Input Manager/InputHandler.cs
@@ -18,15 +18,22 @@ public class InputHandler : MonoBehaviour
 
     void Update()
     {
+        MoveMouse();
+    }
+
+    public void MoveMouse()
+    {
         //pointer to mouse position
+        if (GameManager.Instance.state == GameManager.GameState.Dialogue) return;
         var mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mouseWorldPos.z = 0f;
         Pointer.transform.position = mouseWorldPos;
+        
     }
 
     public void OnClick(InputAction.CallbackContext context)
     {
-        if (!context.started) return;
+        if (!context.started || GameManager.Instance.itemManager.GetInteracting()) return;
 
         var rayHit = Physics2D.GetRayIntersection(_mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue()));
         if (!rayHit.collider) return;
--- Assets/Code/Managers/GameManager.cs
+++ Assets/Code/Managers/GameManager.cs
@@ -7,6 +7,8 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
+    public DialogueManager dialogueManager;
+    public ItemManager itemManager;
 
     public GameState state;
 
