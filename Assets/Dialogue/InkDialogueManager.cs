f88417a0d53e13281f6ec5cf27b076225a00e9d3
IsaacMartos <imartosf@edu.tecnocampus.cat>
2023-11-15 16:16:27 +0100

    tags en proceso

--- Assets/Dialogue/InkDialogueManager.cs
+++ Assets/Dialogue/InkDialogueManager.cs
@@ -27,6 +27,7 @@ public class InkDialogueManager : MonoBehaviour
 
     private const string SPEAKER_TAG = "speaker";
     private const string PORTRAIT_TAG = "portrait";
+    private const string LAYOUT_TAG = "layout";
 
     private bool canContinue = false;
 
@@ -96,14 +97,19 @@ public class InkDialogueManager : MonoBehaviour
     {
         if (currentStory.canContinue)
         {
+<<<<<<< Updated upstream
             //dialogueText.text = currentStory.Continue();
             CleanChoices();
             StartCoroutine(TypeSentence(currentStory.Continue()));
+=======
+            dialogueText.text = currentStory.Continue();
+            DisplayChoices();
+            HandleTags(currentStory.currentTags);
+>>>>>>> Stashed changes
         }
 
         else
         {
-            Debug.Log("saliendo de la historieta");
             ExitDialogueMode();
         }
     }
@@ -118,7 +124,7 @@ public class InkDialogueManager : MonoBehaviour
             yield return new WaitForSeconds(.02f);
         }
         DisplayChoices();
-        //Puedes darle al botón next
+        //Puedes darle al botï¿½n next
         canContinue = true;
         nextButton.SetActive(true);
     }
@@ -131,6 +137,37 @@ public class InkDialogueManager : MonoBehaviour
         for (int i = index; i < choices.Length; i++)
         {
             choices[i].gameObject.SetActive(false);
+        }
+    }
+
+    private void HandleTags(List<string> currentTags)
+    {
+        foreach (string tag in currentTags)
+        {
+            string[] splitTag = tag.Split(":");
+            if (splitTag.Length != 2)
+            {
+                Debug.LogError("Tag could not be apparently parsed: " + tag);
+            }
+
+            string tagKey = splitTag[0].Trim();
+            string tagValue = splitTag[1].Trim();
+
+            switch (tagKey)
+            {
+                case SPEAKER_TAG:
+                    nameText.text = tagValue;
+                    break;
+                case PORTRAIT_TAG:
+                    Debug.Log("portrait" + tagValue);
+                    break;
+                case LAYOUT_TAG:
+                    Debug.Log("layout" + tagValue);
+                    break;
+                default:
+                    Debug.LogWarning("Tag came in but is not currently being handed: " + tag);
+                    break;
+            }
         }
     }
 
