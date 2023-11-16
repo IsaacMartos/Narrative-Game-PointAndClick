e44990664edcb6fa914564f193b0d3e6ef174389
Lucas Montero Pedrosa <57368590+Lucas-Montero-42@users.noreply.github.com>
2023-11-16 16:20:21 +0100

    Regreso al pasado

---
+++ Assets/Code/Managers/TimeManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    public GameObject Past;
+    public GameObject Present;
+    public GameObject Filter;
+    public bool TimePresent = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GoBackTime(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeTraveller();
+
+    }
+
+    private void TimeTraveller()
+    {
+        if (GameManager.Instance.state == GameManager.GameState.Dialogue) return;
+
+        if (!Input.GetKeyDown(KeyCode.Tab)) return;
+
+        if (TimePresent)
+        {
+            GoBackTime(true);
+            return;
+        }
+        GoBackTime(false);
+        // Avisa al game manager para ver si lo has usado en el Acto 2
+    }
+
+    public void GoBackTime(bool back)
+    {
+        TimePresent = !back;
+        Past.SetActive(back);
+        Filter.SetActive(back);
+        Present.SetActive(!back);
+    }
+}
