591c5c871b6b5a27784e1185ded6d90c79f65f67
Lucas Montero Pedrosa <57368590+Lucas-Montero-42@users.noreply.github.com>
2023-12-07 11:35:54 +0100

    CODE

---
+++ Assets/Code/Point and Click/GoToScene.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToScene : ClickableObject
+{
+    public string NextScene;
+    // Start is called before the first frame update
+    public override void Action()
+    {
+        base.Action();
+        Debug.Log("wut");
+        SceneManager.LoadScene(NextScene);
+        
+    }
+}
